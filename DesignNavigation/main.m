02a1345c59e57c625f6b498df66785d5825eaecd
Joshua Howland <development@jkhowland.com>
2014-06-04 09:50:51 -0600

    Initial Commit

---
+++ DesignNavigation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DesignNavigation
+//
+//  Created by Joshua Howland on 6/4/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DNAppDelegate class]));
+    }
+}
